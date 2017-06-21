92b7b62a555ee83a9d2d1f9a30a9d08f19e496b1
Michael J. Wathen <michael_wathen@live.com>
2017-06-21 18:26:59 -0400

    deleted

--- R/imputation.R
+++ R/imputation.R
@@ -1,3 +1,10 @@
+# imputation
+setwd("C:/marpalSVD/R")
+# load("SVD")
+# save.image("SVD")
+library(mice)
+
+# Following is in the image space; no need to run ####
 Data_imput <- mice(fms_subset_2,m=1,seed=500)
 Data_imput_data <- complete(Data_imput,action=1,include=TRUE)
 x<-ifelse(Data_imput_data$NDRM.CH <= 18.2,1,0)
@@ -7,7 +14,7 @@ new_data<- data.frame(x,y )
 dat_y<-new_data[ ,c(2:112)]
 dat_x<-new_data[ ,c(1)]
 
-# loop for all snips
+# loop for all snips ####
 rho <- c(rep(0,111))
 for(i in 1:111){
   x<-dat_x
@@ -18,11 +25,11 @@ rho
 
 
 # Histogram of the rho values
-hist(rho, 
-     main="Rho 2*3", 
-     xlab="Rho", 
-     border="blue", 
+hist(rho,
+     main="Rho 2*3",
+     xlab="Rho",
+     border="blue",
      col="green",
      xlim=c(-0.5,0.5),
-     las=1, 
+     las=1,
      breaks=1)
