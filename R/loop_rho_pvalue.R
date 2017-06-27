cc6b327a81bc8bd7bfb73f1c17c245f4380491f8
Michael J. Wathen <michael_wathen@live.com>
2017-06-27 15:29:59 -0400

    just checking

--- R/loop_rho_pvalue.R
+++ R/loop_rho_pvalue.R
@@ -9,12 +9,14 @@ p_val<- c(rep(0,111))
 for(i in seq(2,112,1)){
   y <- new_data[ ,i]
   rho[i-1] = rho_twoBythree(x,y)
-  p_val[i-1] <- exp(-1397*rho[i-1]^2/2)
+  p_val[i-1] <- exp(-1397*rho[i-1]/2)
 }
 
+
 rho_pval <- data.frame(rho, p_val)
-row.names(rho_pval) <- colnames(new_data)
+row.names(rho_pval) <- colnames(new_data[-1])
 write.table(rho_pval, "rho_pval",
             quote = F,
             row.names = T,
             col.names = T)
+which(rho_pval$p_val <= 0.05)
